f14168e3414746feccda5c5b85aa468a4650b075
Lukiii1993 <lukas.volk@yahoo.de>
2026-08-31 17:22:41 +0200

    Upgrade service worker to v2: network-first for navigation, clear old caches on activate, include root path in precache

--- sw.js
+++ sw.js
@@ -1,31 +1,49 @@
-const CACHE_NAME = "elton-bestellliste-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE_NAME = 'elton-app-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+self.addEventListener('install', (evt) => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve()))
+    )).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const fetchPromise = fetch(event.request)
-        .then((networkResponse) => {
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse.clone()));
-          return networkResponse;
-        })
-        .catch(() => cached);
-      return cached || fetchPromise;
-    })
+self.addEventListener('fetch', (evt) => {
+  const req = evt.request;
+  const url = new URL(req.url);
+
+  if (req.mode === 'navigate' || url.pathname.endsWith('index.html') || url.pathname === '/') {
+    evt.respondWith(
+      fetch(req).then((res) => {
+        caches.open(CACHE_NAME).then((c) => c.put(req, res.clone()));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      caches.open(CACHE_NAME).then((c) => c.put(req, res.clone()));
+      return res;
+    })).catch(() => {})
   );
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
